09e4c210458a176f2f0c77415e2f409608f233b0
Erling Hauan <erling.hauan@digdir.no>
2024-02-01 15:19:30 +0100

    Try to fix pipeline build

--- frontend/form-builder/src/app/App.tsx
+++ frontend/form-builder/src/app/App.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "@digdir/design-system-tokens/brand/digdir/tokens.css";
 import classes from "./App.module.css";
-import { Toolbar } from "../components/Toolbar/";
-import { FormBoard } from "../components/FormBoard/";
-import { SettingsSidebar } from "../components/SettingsSidebar/";
+import { Toolbar } from "../components/Toolbar";
+import { FormBoard } from "../components/FormBoard";
+import { SettingsSidebar } from "../components/SettingsSidebar";
 import { Heading } from "@digdir/design-system-react";
 
 export const App = (): React.JSX.Element => {
