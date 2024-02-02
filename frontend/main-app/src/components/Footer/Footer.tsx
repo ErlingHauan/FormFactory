ecdc00d3f2d53a4883e77eaa84065e43d19965db
Erling Hauan <erling.hauan@digdir.no>
2024-02-02 10:18:45 +0100

    Add background image to footer

--- frontend/main-app/src/components/Footer/Footer.tsx
+++ frontend/main-app/src/components/Footer/Footer.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import classes from "./Footer.module.css";
-import { BranchingIcon } from '@navikt/aksel-icons';
 import { Link } from "@digdir/design-system-react";
 
 export const Footer = (): React.JSX.Element => {
   return (
     <footer className={classes.footer}>
-      <Link href="https://github.com/ErlingHauan/FormFactory"><BranchingIcon />GitHub</Link>
+      <div className={classes.linkContainer}>
+        <Link href="https://github.com/ErlingHauan/FormFactory">
+          Form Factory at GitHub
+        </Link>
+        <Link href="https://aksel.nav.no/">Icons by Aksel</Link>
+        <Link href="https://bgjar.com/">Background pattern by BGJar</Link>
+        <Link href="https://www.designsystemet.no/">
+          Components and colors by Designsystemet
+        </Link>
+      </div>
     </footer>
   );
 };
