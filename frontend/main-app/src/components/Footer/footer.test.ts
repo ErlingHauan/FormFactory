eb5d18589b8b0eaf994dcf65f096aec376e64647
Erling Hauan <erling.hauan@digdir.no>
2024-02-07 13:24:48 +0100

    Testing works, but imports don't

--- frontend/main-app/src/components/Footer/footer.test.ts
+++ frontend/main-app/src/components/Footer/footer.test.ts
@@ -1,7 +1,3 @@
-import {Footer} from "./Footer";
-
-console.log(Footer);
-
 test("1 + 1 = 2", () => {
     expect(1 + 1).toBe(2);
 });
--- frontend/main-app/sum.test.tsx
+++
@@ -1,5 +0,0 @@
-const testSum = require('./sum');
-
-test('adds 1 + 2 to equal 3', () => {
-    expect(testSum(1, 2)).toBe(3);
-});
