be60a5bc720a4a70f11a0ed784a05c6221d0109e
Erling Hauan <erling.hauan@digdir.no>
2024-02-09 14:37:53 +0100

    Fixed BrowserRouter TypeError

--- frontend/main-app/src/index.tsx
+++ frontend/main-app/src/index.tsx
@@ -8,6 +8,6 @@ const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-    <BrowserRouter>
+    <BrowserRouter> 
         <App/>
     </BrowserRouter>);
