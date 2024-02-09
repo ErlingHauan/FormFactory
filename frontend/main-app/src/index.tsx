5e4f88c68e590cc8230e800ead7e2a627ce56284
Erling Hauan <erling.hauan@digdir.no>
2024-02-09 13:05:12 +0100

    BrowserRouter is used in index - but gets typeerror

--- frontend/main-app/src/index.tsx
+++ frontend/main-app/src/index.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import {createRoot} from "react-dom/client";
 import {App} from "./app";
-import {MemoryRouter} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-    <MemoryRouter>
+    <BrowserRouter>
         <App/>
-    </MemoryRouter>);
+    </BrowserRouter>);
