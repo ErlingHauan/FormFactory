372c0f17c7882b55506838c8b3834ca8a92508c9
lassopicasso <lars.walderhaug@digdir.no>
2024-02-20 14:34:31 +0100

    some changes, but need to upg designsystem

--- frontend/main-app/src/components/Header/Header.tsx
+++ frontend/main-app/src/components/Header/Header.tsx
@@ -2,8 +2,35 @@ import React from "react";
 import { Heading, Link } from "@digdir/design-system-react";
 import { ClipboardCheckmarkFillIcon } from "@navikt/aksel-icons";
 import classes from "./Header.module.css";
+import { DropdownMenu } from "@digdir/design-system-react";
+import { MenuHamburgerIcon } from "@navikt/aksel-icons";
 
 export const Header = (): React.JSX.Element => {
+  const isSmallScreen = window.innerWidth < 768;
+
+  if (isSmallScreen) {
+    return (
+      <header className={classes.header}>
+        <Link href="/" className={classes.headerLink}>
+          <Heading level={1} size="medium">
+            Form Factory
+          </Heading>
+          <ClipboardCheckmarkFillIcon className={classes.headerIcon} />
+        </Link>
+        <DropdownMenu size="small">
+          <ul>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/form-builder">Form Builder</Link>
+            </li>
+          </ul>
+        </DropdownMenu>
+      </header>
+    );
+  }
+
   return (
     <header className={classes.header}>
       <Link href="/" className={classes.headerLink}>
