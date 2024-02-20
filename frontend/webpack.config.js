a8e3d75bac2b4cb1c545ed4b633c00c24eedd558
Erling Hauan <erling.hauan@digdir.no>
2024-02-20 12:22:47 +0100

    Make Webpack output static files

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -4,48 +4,59 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 const devServerPorts = require("./devServerPorts.json");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.tsx",
   devtool: "source-map",
   performance: {
-    hints: false,
+    hints: false
   },
   output: {
     path: path.resolve(__dirname, "dist", process.env.npm_package_name),
-    filename: `${process.env.npm_package_name}.js`,
+    filename: `${process.env.npm_package_name}.js`
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: [".tsx", ".ts", ".js"]
   },
   module: {
     rules: [
       {
         test: /\.(ts|tsx)$/,
         use: "ts-loader",
-        exclude: /node_modules/,
+        exclude: /node_modules/
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
+        use: ["style-loader", "css-loader"]
+      }
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./public/index.html",
+      template: "./public/index.html"
     }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "*.{css,svg}",
+          to: "",
+          context: "public",
+        }
+      ]
+    })
   ],
   devServer: {
     hot: true,
     port: devServerPorts[process.env.npm_package_name],
     historyApiFallback: true,
-    client: {
-      overlay: {
-        errors: false,
-        warnings: false,
-        runtimeErrors: true,
-      },
-    },
-  },
+    client:
+      {
+        overlay: {
+          errors: false,
+          warnings: false,
+          runtimeErrors: true
+        }
+      }
+  }
 };
