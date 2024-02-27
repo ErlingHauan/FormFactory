6f3e0a99ac95019755a0647b20ea64e84c5a9069
Erling Hauan <erling.hauan@digdir.no>
2024-02-27 08:39:49 +0100

    Tidy up modal

--- frontend/main-app/src/components/FormModal/FormModal.tsx
+++ frontend/main-app/src/components/FormModal/FormModal.tsx
@@ -1,27 +1,28 @@
-import classes from "./FormModal.module.css"
+import classes from "./FormModal.module.css";
 import { Button, Divider, Heading, Modal, Paragraph } from "@digdir/design-system-react";
 import React, { ReactNode } from "react";
 
 interface FormModalProps {
   children: ReactNode;
 }
-export const FormModal: React.FC<FormModalProps> = ({children}) => {
+
+export const FormModal: React.FC<FormModalProps> = ({ children }) => {
   return (
     <Modal.Root>
       <Modal.Trigger><Button>{children}</Button></Modal.Trigger>
       <Modal.Dialog className={classes.modalWindow}>
         <Modal.Header>
-          <Heading spacing>
+          <Heading level={1} size="large" spacing>
             Responses
           </Heading></Modal.Header>
         <Modal.Content>
-          <Heading>Response 1</Heading>
+          <Heading level={2} size="medium" spacing>Response 1</Heading>
           <Heading level={6} size="small">What is your favorite animal?</Heading>
           <Paragraph spacing={true}>Cat</Paragraph>
           <Heading level={6} size="small">What is your 2nd favorite animal?</Heading>
           <Paragraph spacing={true}>Dog</Paragraph>
-          <Divider />
-          <Heading>Response 2</Heading>
+          <Divider className={classes.spacing}/>
+          <Heading level={2} size="medium" spacing>Response 2</Heading>
           <Heading level={6} size="small">What is your favorite animal?</Heading>
           <Paragraph spacing={true}>Dog</Paragraph>
           <Heading level={6} size="small">What is your 2nd favorite animal?</Heading>
@@ -29,5 +30,5 @@ export const FormModal: React.FC<FormModalProps> = ({children}) => {
         </Modal.Content>
       </Modal.Dialog>
     </Modal.Root>
-  )
-}
+  );
+};
