0675f30df3c5fd6b82b2801a374986fd26e71dd3
Erling Hauan <erling.hauan@digdir.no>
2024-02-27 15:12:59 +0100

    Make dashboard loop through a JS object

--- frontend/main-app/src/components/Dashboard/Dashboard.tsx
+++ frontend/main-app/src/components/Dashboard/Dashboard.tsx
@@ -10,10 +10,46 @@ import {
   TrashFillIcon
 } from "@navikt/aksel-icons";
 import { FormModal } from "../FormModal/FormModal";
+import { useTranslation } from "react-i18next";
 
 // Note: Currently this component contains placeholder data. Later, it will load its data dynamically from the database.
 
+const forms = [
+  {
+    id: 1,
+    title: "Form 1",
+    description: "A survey of the users of Form Factory.",
+    status: "Published - June 26th 2024",
+    expirationDate: "Not set",
+    submissions: 19
+  },
+  {
+    id: 2,
+    title: "Form 2",
+    description: "A survey of the users of other form.",
+    status: "Published - June 32th 2028",
+    expirationDate: "Not set",
+    submissions: 16
+  }
+];
+
+interface CustomParagraphProps {
+  heading: string;
+  content: string | number;
+}
+
+const CustomParagraph: React.FC<CustomParagraphProps> = ({ heading, content }) => (
+  <>
+    <Heading level={6} size="small">
+      {heading}
+    </Heading>
+    <Paragraph spacing={true}>{content}</Paragraph>
+  </>
+);
+
 export const Dashboard = (): React.JSX.Element => {
+  const { t } = useTranslation();
+
   return (
     <main className={classes.dashboard}>
       <div className={classes.headingContainer}>
@@ -22,65 +58,36 @@ export const Dashboard = (): React.JSX.Element => {
         </Heading>
         <Button color="success" asChild>
           <a href="/form-builder">
-            <FilePlusFillIcon />New form
+            <FilePlusFillIcon />{t("dashboard.new_form")}
           </a>
         </Button>
       </div>
-      <Accordion border={true}>
-        <Accordion.Item>
-          <Accordion.Header>
-            <Heading level={2} size="medium">Form 1</Heading>
-          </Accordion.Header>
-          <Accordion.Content className={classes.accordionContentContainer}>
-            <div className={classes.infoContainer}>
-              <Heading level={6} size="small">Description</Heading>
-              <Paragraph spacing={true}>A survey of the users of Form Factory.</Paragraph>
-              <Heading level={6} size="small">Status</Heading>
-              <Paragraph spacing={true}>Published - June 26th 2024</Paragraph>
-              <Heading level={6} size="small">Expiration date</Heading>
-              <Paragraph spacing={true}>Not set</Paragraph>
-              <Heading level={6} size="small">Submissions</Heading>
-              <Paragraph spacing={true}>19</Paragraph>
-            </div>
-            <div className={classes.buttonContainer}>
-              <Button><ClipboardLinkFillIcon />Share form</Button>
-              <FormModal>
-                <PersonEnvelopeFillIcon />View responses
-              </FormModal>
-              <Button><CloudDownFillIcon />Download data</Button>
-              <Button color="danger"><TrashFillIcon />Delete form</Button>
-            </div>
-          </Accordion.Content>
-        </Accordion.Item>
-      </Accordion>
 
-      <Accordion border={true}>
-        <Accordion.Item>
-          <Accordion.Header>
-            <Heading level={2} size="medium">Form 2</Heading>
-          </Accordion.Header>
-          <Accordion.Content className={classes.accordionContentContainer}>
-            <div className={classes.infoContainer}>
-              <Heading level={6} size="small">Description</Heading>
-              <Paragraph spacing={true}>A survey of the users of Form Factory.</Paragraph>
-              <Heading level={6} size="small">Status</Heading>
-              <Paragraph spacing={true}>Published - June 26th 2024</Paragraph>
-              <Heading level={6} size="small">Expiration date</Heading>
-              <Paragraph spacing={true}>Not set</Paragraph>
-              <Heading level={6} size="small">Submissions</Heading>
-              <Paragraph spacing={true}>19</Paragraph>
-            </div>
-            <div className={classes.buttonContainer}>
-              <Button><ClipboardLinkFillIcon />Share form</Button>
-              <FormModal>
-                <PersonEnvelopeFillIcon />View responses
-              </FormModal>
-              <Button><CloudDownFillIcon />Download data</Button>
-              <Button color="danger"><TrashFillIcon />Delete form</Button>
-            </div>
-          </Accordion.Content>
-        </Accordion.Item>
-      </Accordion>
+      {forms.map((form) => (
+        <Accordion border={true} key={form.id}>
+          <Accordion.Item>
+            <Accordion.Header>
+              <Heading level={2} size="medium">{form.title}</Heading>
+            </Accordion.Header>
+            <Accordion.Content className={classes.accordionContentContainer}>
+              <div className={classes.infoContainer}>
+                <CustomParagraph heading="Description" content={form.description} />
+                <CustomParagraph heading="Status" content={form.status} />
+                <CustomParagraph heading="Expiration date" content={form.expirationDate} />
+                <CustomParagraph heading="Submissions" content={form.submissions} />
+              </div>
+              <div className={classes.buttonContainer}>
+                <Button><ClipboardLinkFillIcon />Share form</Button>
+                <FormModal>
+                  <PersonEnvelopeFillIcon />View responses
+                </FormModal>
+                <Button><CloudDownFillIcon />Download data</Button>
+                <Button color="danger"><TrashFillIcon />Delete form</Button>
+              </div>
+            </Accordion.Content>
+          </Accordion.Item>
+        </Accordion>
+      ))}
     </main>
   );
 };
