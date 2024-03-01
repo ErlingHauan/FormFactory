b39afe55492a5b8f69004bb6b2b3cdef845a1f26
Erling Hauan <erling.hauan@digdir.no>
2024-03-01 15:29:31 +0100

    Add test for Get

--- backend/tests/Controllers/UsersControllerTests.cs
+++ backend/tests/Controllers/UsersControllerTests.cs
@@ -41,4 +41,35 @@ public class UsersControllerTests
         var returnedUsers = Assert.IsType<List<UserDto>>(actionResult.Value);
         Assert.Equal(2, returnedUsers.Count);
     }
+    
+    // Write a GetAll test that should receive "not found" if the database is empty
+    
+    // Write a Get test that should return a user successfully
+    
+    [Fact]
+    public async Task Get_UserNotFound_ReturnsNotFound()
+    {
+        // Arrange
+        _mockRepo.Setup(repo => repo.Get(It.IsAny<int>())).ReturnsAsync((UserEntity)null);
+        
+        // Act
+        var result = await _controller.Get(1);
+        
+        // Assert
+        Assert.IsType<NotFoundObjectResult>(result.Result);
+    }
+    
+    // Write a test that should create a user successfully
+    
+    // Write a test that do not create a user if data is missing
+    
+    // Write a test that should update a user successfully
+    
+    // Write a test that does not find the provided user
+    
+    // Write a test that can't update the user due to missing information
+    
+    // Write a test that logs in the user successfully
+    
+    // Write a test that can't log in the user due to wrong email/password combination
 }
