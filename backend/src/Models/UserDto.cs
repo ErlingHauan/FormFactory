978931f61c2073af92db8d0ca059e91f1720c488
Erling Hauan <148075168+ErlingHauan@users.noreply.github.com>
2024-03-08 08:14:49 +0100

    Add backend validation to UserDto (#115)

--- backend/src/Models/UserDto.cs
+++ backend/src/Models/UserDto.cs
@@ -1,3 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace FormAPI.Models;
 
-public record UserDto(int Id, string? Email, string? Password, string? Organization);
+public record UserDto
+{
+    public UserDto(int id, string email, string password, string? organization)
+    {
+        Id = id;
+        Email = email;
+        Password = password;
+        Organization = organization;
+    }
+
+    [Required]
+    public int Id { get; init; }
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; init; }
+
+    [Required]
+    [MinLength(8)]
+    public string Password { get; init; }
+
+    public string? Organization { get; init; }
+}
