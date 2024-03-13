5e825ce59a17e97ef87f4a2728af91379bb19a84
Erling Hauan <erling.hauan@digdir.no>
2024-03-13 08:21:28 +0100

    Improve FormModal styling

--- frontend/main-app/src/components/FormModal/FormModal.tsx
+++ frontend/main-app/src/components/FormModal/FormModal.tsx
@@ -1,26 +1,23 @@
 import classes from "./FormModal.module.css";
-import { Divider, Heading, Modal } from "@digdir/design-system-react";
+import { Heading, Modal } from "@digdir/design-system-react";
 import React, { ReactNode } from "react";
 import submittedData from "./submittedData.json";
-import { CustomParagraph } from "../CustomParagraph/CustomParagraph";
+import { CustomParagraph } from "../CustomParagraph";
 
 const ListAnswers = (): React.JSX.Element => {
   const submissions = submittedData.submissions;
 
   return (
     <>
-      {submissions.map(({ submissionId, answers }, submissionIndex) => (
-        <React.Fragment key={submissionId}>
-          <Heading level={3} size="large" spacing>
+      {submissions.map(({ submissionId, answers })  => (
+        <div key={submissionId} className={classes.submission}>
+          <Heading level={3} size="medium" className={classes.submissionHeading}>
             Submission #{submissionId}
           </Heading>
           {answers.map(({ answerId, question, answer }) => (
             <CustomParagraph key={answerId} heading={question} content={answer} />
           ))}
-          {submissionIndex < submissions.length - 1 && (
-            <Divider color="strong" className={classes.spacing} />
-          )}
-        </React.Fragment>
+        </div>
       ))}
     </>
   );
@@ -40,8 +37,8 @@ export const FormModal: React.FC<FormModalProps> = ({ children, className, size,
         {children}
       </Modal.Trigger>
       <Modal.Dialog className={classes.modalWindow}>
-        <Modal.Header>Submissions to {submittedData.title}</Modal.Header>
-        <Modal.Content>
+        <Modal.Header className={classes.modalHeader}>Submissions to {submittedData.title}</Modal.Header>
+        <Modal.Content className={classes.modalContent}>
           <ListAnswers />
         </Modal.Content>
       </Modal.Dialog>
