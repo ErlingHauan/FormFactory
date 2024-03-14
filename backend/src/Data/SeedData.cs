93de3f394531c5f882e82ff9fb039c635deed0fc
Erling Hauan <erling.hauan@digdir.no>
2024-03-14 13:54:15 +0100

    Fix object creation style

--- backend/src/Data/SeedData.cs
+++ backend/src/Data/SeedData.cs
@@ -9,21 +9,21 @@ public static class SeedData
     {
         builder.Entity<UserEntity>().HasData(new List<UserEntity>
         {
-            new UserEntity
+            new()
             {
                 Id = 1,
                 Email = "a@a.com",
                 Password = "12345678",
                 Organization = "A.com"
             },
-            new UserEntity
+            new()
             {
                 Id = 2,
                 Email = "johnny@testepartementet.no",
                 Password = "johnny123",
                 Organization = "Testdepartementet"
             },
-            new UserEntity
+            new()
             {
                 Id = 3,
                 Email = "test@test.com",
@@ -72,14 +72,14 @@ public static class SeedData
 
         var responseList = new List<SubmissionResponse>
         {
-            new SubmissionResponse()
+            new()
             {
                 Name = "question1",
                 Label = "Question 1",
                 Order = 0,
                 Response = "Yes, I agree"
             },
-            new SubmissionResponse()
+            new()
             {
                 Name = "question2",
                 Label = "Question 2",
@@ -90,14 +90,14 @@ public static class SeedData
 
         builder.Entity<SubmissionEntity>().HasData(new List<SubmissionEntity>
         {
-            new SubmissionEntity()
+            new()
             {
                 Id = Guid.NewGuid(),
                 Submitted = DateTimeOffset.UtcNow,
                 Responses = responseList
             },
 
-            new SubmissionEntity()
+            new()
             {
                 Id = Guid.NewGuid(),
                 Submitted = DateTimeOffset.UtcNow,
