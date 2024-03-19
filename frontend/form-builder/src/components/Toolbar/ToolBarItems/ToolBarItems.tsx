2ab3c0ca96ebb1ea6fa222211638e3bff3cb770d
lassopicasso <lars.walderhaug@digdir.no>
2024-03-19 12:06:28 +0100

    Separate the draggable items

--- frontend/form-builder/src/components/Toolbar/ToolBarItems/ToolBarItems.tsx
+++ frontend/form-builder/src/components/Toolbar/ToolBarItems/ToolBarItems.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useDrag } from "react-dnd";
+import { DraggableItemsType } from "../../../types/dndTypes";
 import {
   BulletListIcon,
   TasklistSaveIcon,
@@ -6,6 +8,7 @@ import {
   MenuHamburgerIcon,
 } from "@navikt/aksel-icons";
 import { useTranslation } from "react-i18next";
+import classes from "./ToolBarItems.module.css";
 
 export const ToolBarItems = () => {
   const { t } = useTranslation();
@@ -14,29 +17,69 @@ export const ToolBarItems = () => {
     {
       icon: <TasklistSaveIcon fontSize="3rem" />,
       text: t("toolbar_tools.save.form.component"),
+      draggable: false,
     },
     {
       icon: <TasklistSendIcon fontSize="3rem" />,
       text: t("toolbar_tools.validate.form.component"),
+      draggable: false,
     },
     {
       icon: <MenuHamburgerIcon fontSize="3rem" />,
       text: t("toolbar_tools.text.field.component"),
+      draggable: true,
     },
     {
       icon: <BulletListIcon fontSize="3rem" />,
       text: t("toolbar_tools.multiple.choice.component"),
+      draggable: true,
     },
   ];
 
   return (
-    <div>
-      {Items.map((item, index) => (
-        <div key={index}>
-          {item.icon}
-          <span>{item.text}</span>
-        </div>
-      ))}
+    <>
+      {Items.map((item, index) =>
+        item.draggable ? (
+          <DraggableItem key={index} item={item} index={index} />
+        ) : (
+          <div key={index} className={classes.toolbarIcon}>
+            {item.icon}
+            <div>{item.text}</div>
+          </div>
+        ),
+      )}
+    </>
+  );
+};
+
+interface DraggableItemProps {
+  item: {
+    icon: React.ReactNode;
+    text: string;
+  };
+  index: number;
+}
+
+const DraggableItem: React.FC<DraggableItemProps> = ({ item, index }) => {
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: DraggableItemsType.ToolbarItem,
+    item: { ...item, index },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  }));
+
+  return (
+    <div
+      ref={drag}
+      className={classes.toolbarIcon}
+      style={{
+        opacity: isDragging ? 0.5 : 1,
+        cursor: "move",
+      }}
+    >
+      {item.icon}
+      <div>{item.text}</div>
     </div>
   );
 };
--- frontend/form-builder/src/components/Toolbar/Toolbar.tsx
+++ frontend/form-builder/src/components/Toolbar/Toolbar.tsx
@@ -1,40 +1,24 @@
 import { Heading } from "@digdir/design-system-react";
 
 import React from "react";
-import classes from "./Toolbar.module.css";
 import { useTranslation } from "react-i18next";
-import { useDrag } from "react-dnd";
-import { DraggableItemsType } from "../../types/dndTypes";
 import { ToolBarItems } from "./ToolBarItems/ToolBarItems";
 
 export const Toolbar = (): React.JSX.Element => {
   const { t } = useTranslation();
 
-  const [{ isDragging }, drag] = useDrag(() => ({
-    item: { type: DraggableItemsType.ToolbarItem },
-    collect: (monitor) => ({
-      isDragging: !!monitor.isDragging(),
-    }),
-    type: DraggableItemsType.ToolbarItem,
-  }));
-
   return (
-    <div
-      style={{
-        opacity: isDragging ? 0.5 : 1,
-        cursor: "move",
-      }}
-    >
+    <>
       <Heading level={2} size="medium" spacing>
         {t("toolbar_tools")}
       </Heading>
       <ul>
-        <div className={classes.toolbarIcon} ref={drag}>
+        <div>
           <li>
             <ToolBarItems />
           </li>
         </div>
       </ul>
-    </div>
+    </>
   );
 };
