54f09f24b0f969204d91a9216c6383c4bd6d131a
lassopicasso <lars.walderhaug@digdir.no>
2024-03-22 13:37:17 +0100

    add share form button

--- frontend/main-app/src/components/DashboardAccordion/ShareForm.tsx
+++ frontend/main-app/src/components/DashboardAccordion/ShareForm.tsx
@@ -1,47 +1,16 @@
 import React, { useState, useEffect } from "react";
-import { Button, Popover } from "@digdir/design-system-react";
-import { ClipboardLinkFillIcon, CheckmarkIcon, FileTextIcon } from "@navikt/aksel-icons";
+import { Button } from "@digdir/design-system-react";
+import { ClipboardLinkFillIcon, CheckmarkIcon } from "@navikt/aksel-icons";
 import { useTranslation } from "react-i18next";
 import classes from "./DashboardAccordion.module.css";
 
-interface ShareFormProps {
-  id: number;
+interface ShareForm {
+  formUrl: string;
 }
 
-export const ShareForm: React.FC<ShareFormProps> = ({ id }) => {
+export const ShareForm: React.FC<ShareForm> = ({ formUrl }) => {
   const [isLinkCopied, setIsLinkCopied] = useState(false);
   const { t } = useTranslation();
-
-  const copyLink = (id: number) => {
-    const url = `${window.location.origin}/view/${id}`;
-    navigator.clipboard.writeText(url);
-  };
-
-  const CopyLinkButton = () => {
-    return (
-      <Button
-        size="small"
-        variant="secondary"
-        onClick={() => {
-          setIsLinkCopied(true);
-          copyLink(id);
-        }}
-        color={isLinkCopied ? "success" : "first"}
-      >
-        {isLinkCopied ? (
-          <>
-            <CheckmarkIcon />
-            {t("dashboard.share.form.copied")}
-          </>
-        ) : (
-          <>
-            <ClipboardLinkFillIcon />
-            {t("dashboard.share.form")}
-          </>
-        )}
-      </Button>
-    );
-  };
 
   useEffect(() => {
     if (isLinkCopied) {
@@ -53,20 +22,27 @@ export const ShareForm: React.FC<ShareFormProps> = ({ id }) => {
   }, [isLinkCopied]);
 
   return (
-    <Popover variant="info">
-      <Popover.Trigger variant="secondary" size="small" className={classes.button}>
-        <ClipboardLinkFillIcon />
-        {t("dashboard.share.form")}
-      </Popover.Trigger>
-      <Popover.Content>
-        <div className={classes.popoverButtons}>
-          <Button size="small" variant="secondary">
-            <FileTextIcon />
-            {t("dashboard.view.form")}
-          </Button>
-          <CopyLinkButton />
-        </div>
-      </Popover.Content>
-    </Popover>
+    <Button
+      size="small"
+      variant="secondary"
+      onClick={() => {
+        setIsLinkCopied(true);
+        navigator.clipboard.writeText(formUrl);
+      }}
+      color={isLinkCopied ? "success" : "first"}
+      className={classes.button}
+    >
+      {isLinkCopied ? (
+        <>
+          <CheckmarkIcon />
+          {t("dashboard.share.form.copied")}
+        </>
+      ) : (
+        <>
+          <ClipboardLinkFillIcon />
+          {t("dashboard.share.form")}
+        </>
+      )}
+    </Button>
   );
 };
