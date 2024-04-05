d1b8d1e45c0034e45e1581aec7016bc1ad71f600
Erling Hauan <148075168+ErlingHauan@users.noreply.github.com>
2024-04-05 07:27:20 +0200

    Form Builder preview scrollbar (#150)

---
+++ frontend/form-builder/src/components/FormPreview/FormPreview.tsx
@@ -0,0 +1,59 @@
+import { Heading, Paragraph } from "@digdir/design-system-react";
+import React from "react";
+import { ComponentIcon, XMarkIcon } from "@navikt/aksel-icons";
+import classes from "./FormPreview.module.css";
+import { useTranslation } from "react-i18next";
+import { useDrop } from "react-dnd";
+import { DraggableItemsType } from "../../types/dndTypes";
+import { ItemProps } from "../Toolbar/DraggableItem";
+
+export const FormPreview = (): React.JSX.Element => {
+  const { t } = useTranslation();
+  const [droppedItems, setDroppedItems] = React.useState<string[]>([]);
+
+  const [{ isOver }, drop] = useDrop(() => ({
+    accept: DraggableItemsType.ToolbarItem,
+    drop: (item: ItemProps) => {
+      setDroppedItems((prev) => [...prev, item.text]);
+    },
+    collect: (monitor) => ({
+      isOver: !!monitor.isOver(),
+    }),
+  }));
+
+  const handleRemoveItem = (index: number) => {
+    const newItems = droppedItems.filter((_, i) => i !== index);
+    setDroppedItems(newItems);
+  };
+
+  return (
+    <>
+      <Heading level={3} size="xxsmall" className={classes.previewHeading}>
+        {t("form_builder.preview")}
+      </Heading>
+      <div
+        ref={drop}
+        className={classes.dropArea}
+        style={{ backgroundColor: isOver && "lightgreen" }}
+      >
+        {droppedItems.length === 0 ? (
+          <>
+            <ComponentIcon className={classes.noComponentsIcon} />
+            <Paragraph>{t("form_builder.drag.component.message")}</Paragraph>
+          </>
+        ) : (
+          droppedItems.map((item, index) => (
+            <div key={index} className={classes.droppedItem}>
+              <div>{item}</div>
+              <XMarkIcon
+                title="Remove item"
+                className={classes.removalItem}
+                onClick={() => handleRemoveItem(index)}
+              />
+            </div>
+          ))
+        )}
+      </div>
+    </>
+  );
+};
--- frontend/form-builder/src/components/SettingsSidebar/SettingsSidebar.tsx
+++ frontend/form-builder/src/components/SettingsSidebar/SettingsSidebar.tsx
@@ -6,25 +6,29 @@ import { useTranslation } from "react-i18next";
 export const SettingsSidebar = (): React.JSX.Element => {
   const { t } = useTranslation();
   return (
-    <div className={classes.settingsSidebar}>
-      <Heading level={3} size="xxsmall" spacing>
-        {t("settings_side_bar")}
-      </Heading>
-      <Textfield
-        label={t("settings_side_bar.component.label")}
-        size="small"
-        className={classes.spacing}
-      />
-      <Textfield
-        label={t("settings_side_bar.minimum.length")}
-        size="small"
-        className={classes.spacing}
-      />
-      <Textfield
-        label={t("settings_side_bar.maximum.length")}
-        size="small"
-        className={classes.spacing}
-      />
-    </div>
+    <>
+      <div className={classes.settingsHeading}>
+        <Heading level={3} size="xxsmall">
+          {t("settings_side_bar")}
+        </Heading>
+      </div>
+      <div className={classes.settingsContent}>
+        <Textfield
+          label={t("settings_side_bar.component.label")}
+          size="small"
+          className={classes.spacing}
+        />
+        <Textfield
+          label={t("settings_side_bar.minimum.length")}
+          size="small"
+          className={classes.spacing}
+        />
+        <Textfield
+          label={t("settings_side_bar.maximum.length")}
+          size="small"
+          className={classes.spacing}
+        />
+      </div>
+    </>
   );
 };
