b496237890504c8b37e3fd79ec154c065960decb
Erling Hauan <erling.hauan@digdir.no>
2024-04-08 13:40:26 +0200

    Create required fields

--- frontend/form-builder/src/app/App.tsx
+++ frontend/form-builder/src/app/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { createContext, useEffect, useRef, useState } from "react";
 import "@digdir/design-system-tokens/brand/digdir/tokens.css";
 import classes from "./App.module.css";
 import { Toolbar } from "../components/Toolbar";
@@ -8,12 +8,16 @@ import { HTML5Backend } from "react-dnd-html5-backend";
 import { useAuthorization } from "../../../main-app/src/hooks/useAuthorization";
 import { CompSettings } from "../components/CompSettings";
 
+export const ComponentContext = createContext(null);
+
 export const App = (): React.JSX.Element => {
   const [formComponents, setFormComponents] = useState<FormComponent[]>([]);
   useAuthorization();
   const settingsRef = useRef<HTMLDialogElement>(null);
   const [windowSize, setWindowSize] = React.useState(window.innerWidth);
   const isSmallScreen = windowSize < 768;
+
+  const [currentComponent, setCurrentComponent] = useState<FormComponent | null>();
 
   useEffect(() => {
     const handleResize = () => {
@@ -24,22 +28,31 @@ export const App = (): React.JSX.Element => {
   }, []);
 
   return (
-    <div className={classes.formBuilder}>
-      <DndProvider backend={HTML5Backend}>
-        <div className={classes.builderSection}>
-          <Toolbar />
+    <ComponentContext.Provider
+      value={{
+        formComponents,
+        setFormComponents,
+        currentComponent,
+        setCurrentComponent,
+      }}
+    >
+      <div className={classes.formBuilder}>
+        <DndProvider backend={HTML5Backend}>
+          <div className={classes.builderSection}>
+            <Toolbar />
+          </div>
+          <div className={classes.builderSection}>
+            <FormPreview
+              settingsRef={settingsRef}
+              formComponents={formComponents}
+              setFormComponents={setFormComponents}
+            />
+          </div>
+        </DndProvider>
+        <div className={isSmallScreen ? classes.builderModal : classes.builderSection}>
+          <CompSettings isSmallScreen={isSmallScreen} settingsRef={settingsRef} />
         </div>
-        <div className={classes.builderSection}>
-          <FormPreview
-            settingsRef={settingsRef}
-            formComponents={formComponents}
-            setFormComponents={setFormComponents}
-          />
-        </div>
-      </DndProvider>
-      <div className={isSmallScreen ? classes.builderModal : classes.builderSection}>
-        <CompSettings isSmallScreen={isSmallScreen} settingsRef={settingsRef} />
       </div>
-    </div>
+    </ComponentContext.Provider>
   );
 };
--- frontend/form-builder/src/components/CompSettings/CompSettings.tsx
+++ frontend/form-builder/src/components/CompSettings/CompSettings.tsx
@@ -1,9 +1,11 @@
-import { Textfield } from "@digdir/design-system-react";
-import React from "react";
+import { Button, Checkbox, Radio, Textarea, Textfield } from "@digdir/design-system-react";
+import React, { useContext, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { CompSettingsModal } from "./CompSettingsModal";
 import { CompSettingsSidebar } from "./CompSettingsSideBar";
 import classes from "./CompSettings.module.css";
+import { ComponentContext } from "../../app/App";
+import { TrashFillIcon } from "@navikt/aksel-icons";
 
 interface CompSettingsProps {
   isSmallScreen: boolean;
@@ -14,14 +16,13 @@ export const CompSettings = ({
   isSmallScreen,
   settingsRef,
 }: CompSettingsProps): React.JSX.Element => {
-  const { t } = useTranslation();
-
   const SettingsContent = () => {
+    const { currentComponent } = useContext(ComponentContext);
     return (
       <div className={classes.compSettingsContent}>
-        <Textfield label={t("settings_side_bar.component.label")} size="small" />
-        <Textfield label={t("settings_side_bar.minimum.length")} size="small" />
-        <Textfield label={t("settings_side_bar.maximum.length")} size="small" />
+        <InputFieldSettings />
+        {currentComponent?.type === "inputField" && <InputFieldSettings />}
+        {currentComponent?.type === "radio" && <RadioSettings />}
       </div>
     );
   };
@@ -30,5 +31,87 @@ export const CompSettings = ({
     <CompSettingsModal SettingsContent={SettingsContent} settingsRef={settingsRef} />
   ) : (
     <CompSettingsSidebar SettingsContent={SettingsContent} />
+  );
+};
+
+const InputFieldSettings = () => {
+  const { t } = useTranslation();
+  const [inputType, setInputType] = useState<"Text" | "Number">("Text");
+
+  const handleInputFormat = (value: string) => {
+    setInputType(value as "Text" | "Number");
+  };
+
+  return (
+    <>
+      <Textfield label="Name" size="small" placeholder={"Required"} />
+      <Textfield
+        label={t("settings_side_bar.component.label")}
+        size="small"
+        placeholder={"Required"}
+      />
+      <Checkbox size="small" value="required">
+        Require user response
+      </Checkbox>
+      <Radio.Group
+        defaultValue="Text"
+        size="small"
+        legend="Input format"
+        onChange={handleInputFormat}
+      >
+        <Radio value="Text">Text</Radio>
+        <Radio value="Number">Number</Radio>
+      </Radio.Group>
+      {inputType === "Text" && <TextSettings />}
+      {inputType === "Number" && <NumberSettings />}
+      <Button color={"danger"} variant={"secondary"} size={"small"}>
+        <TrashFillIcon />
+        Delete component
+      </Button>
+    </>
+  );
+};
+
+const TextSettings = () => {
+  const { t } = useTranslation();
+  return (
+    <>
+      <Textfield
+        label={t("settings_side_bar.minimum.length")}
+        size="small"
+        placeholder="Optional"
+      />
+      <Textfield
+        label={t("settings_side_bar.maximum.length")}
+        size="small"
+        placeholder="Optional"
+      />
+    </>
+  );
+};
+
+const NumberSettings = () => {
+  const { t } = useTranslation();
+  return (
+    <>
+      <Textfield label={t("settings_side_bar.minimum.value")} size="small" placeholder="Optional" />
+      <Textfield label={t("settings_side_bar.maximum.value")} size="small" placeholder="Optional" />
+    </>
+  );
+};
+
+const RadioSettings = () => {
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <Textfield label="Name" size="small" />
+      <Textfield label={t("settings_side_bar.component.label")} size="small" />
+      <Textarea label={"Choices (comma separated)"} size="small" />
+      <Button color={"danger"} variant={"secondary"} size={"small"}>
+        <TrashFillIcon />
+        Delete component
+      </Button>
+    </>
   );
 };
